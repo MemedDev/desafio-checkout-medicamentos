6467368dc8c8bf30acbae570e781bf68a1e06ad6
Carlos Henrique Rabelo <cid360@hotmail.com>
2020-02-21 23:42:31 -0300

    feat: link pages

--- components/FindDrugstore/FindDrugstore.js
+++ components/FindDrugstore/FindDrugstore.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Link from "next/link";
+import querystring from "querystring";
+
 import style from "./FindDrugstore.scss";
 
+const coord = {
+  lat: -23.5648304,
+  lon: -46.6436604
+};
 const FindDrugstore = () => (
-  <div className={style.find}>
-    <b>Encontrar Farmácias</b>
-    <br />
-    <span>Serviço oferecido por Memed</span>
-  </div>
+  <Link href={`/drugstores?${querystring.stringify(coord)}`}>
+    <div className={style.find}>
+      <b>Encontrar Farmácias</b>
+      <br />
+      <span>Serviço oferecido por Memed</span>
+    </div>
+  </Link>
 );
 export default FindDrugstore;
